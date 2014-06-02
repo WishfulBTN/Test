d69c7e3d2d8ddd15508360fe415aea12b6d5bb3f
WishfulBTN <wishfulbtn@gmail.com>
2014-06-02 06:43:47 +0200

    Added banking system and dice game

---
+++ willie/bank.py
@@ -0,0 +1,160 @@
+from willie import module
+import MySQLdb as mdb
+import sys
+import re
+
+staff = 10
+
+@module.commands('bal')
+def bal (bot, trigger):
+	try:
+		con = mdb.connect('localhost', 'casino', 'btncasino', 'casino');
+	except:
+		print "Error: Unable to connect to user settings DB."
+		bot.say("Error: Unable to connect to DB.")
+		return
+	
+	if trigger.group(2):
+		nick = re.sub("[^a-zA-Z0-9]","", trigger.group(2))
+		if nick == '':
+			nick = trigger.nick
+	else:
+		nick = trigger.nick
+	
+	if nick:
+		cur = con.cursor()
+		cur.execute("SELECT u.nick, w.balance FROM user u, wallet w WHERE u.nick = '" + nick + "' AND w.id = u.btn_id")
+		row = cur.fetchone()
+		if row is None:
+			bot.say(nick + " does not exist in the database.")
+			return
+		else:
+			nick = str(row[0])
+			balance = str(row[1])
+			bot.say(nick + " has a balance of " + balance)
+			return
+		con.close()
+
+@module.commands('credit')
+def credit (bot, trigger):
+	if trigger.group(3) and trigger.group(4):
+		tnick, tamt = trigger.group(2).split(None, 1)
+		nick = re.sub("[^a-zA-Z0-9]","", tnick)
+		amt = re.sub("[^0-9]","", tamt)
+	else:
+		bot.say("Please enter in format of .credit <user> <amount>")
+		return
+	try:
+		con = mdb.connect('localhost', 'casino', 'btncasino', 'casino');
+	except:
+		print "Error: Unable to connect to user settings DB."
+		bot.say("Error: Unable to connect to DB.")
+		return
+	
+	if nick and amt:
+		cur = con.cursor()
+		cur.execute("SELECT role FROM user WHERE nick = '" + trigger.nick + "';")
+		row = cur.fetchone()
+	if int(row[0]) >= staff:
+		cur.execute("SELECT btn_id FROM user WHERE nick = '" + nick + "';")
+		row = cur.fetchone()
+		if row is None:
+			bot.say(nick + " does not exist in the database.")
+			return
+		else:
+			id = str(row[0])
+			cur.execute("UPDATE wallet SET balance = balance + " + amt + " WHERE id = '" + id + "';")
+			con.commit()
+			cur.execute("SELECT balance FROM wallet WHERE id = '" + id + "';")
+			row = cur.fetchone()
+			balance = str(row[0])
+			bot.say(amt + " credited into " + nick + "'s account. New balance: " + balance)
+			return
+		return
+	else:
+		bot.say("Please enter in format of .credit <user> <amount>")
+		return
+	con.close()	
+
+@module.commands('debit')
+def debit (bot, trigger):
+	if trigger.group(3) and trigger.group(4):
+		tnick, tamt = trigger.group(2).split(None, 1)
+		nick = re.sub("[^a-zA-Z0-9]","", tnick)
+		amt = re.sub("[^0-9]","", tamt)
+	else:
+		bot.say("Please enter in format of .debit <user> <amount>")
+		return
+	try:
+		con = mdb.connect('localhost', 'casino', 'btncasino', 'casino');
+	except:
+		print "Error: Unable to connect to user settings DB."
+		bot.say("Error: Unable to connect to DB.")
+		return
+	
+	if nick and amt:
+		cur = con.cursor()
+		cur.execute("SELECT role FROM user WHERE nick = '" + trigger.nick + "';")
+		row = cur.fetchone()
+		if int(row[0]) >= staff:
+			cur.execute("SELECT btn_id FROM user WHERE nick = '" + nick + "';")
+			row = cur.fetchone()
+			if row is None:
+				bot.say(nick + " does not exist in the database.")
+				return
+			else:
+				id = str(row[0])
+				cur.execute("UPDATE wallet SET balance = balance - " + amt + " WHERE id = '" + id + "';")
+				con.commit()
+				cur.execute("SELECT balance FROM wallet WHERE id = '" + id + "';")
+				row = cur.fetchone()
+				balance = str(row[0])
+				bot.say(amt + " debited from " + nick + "'s account. New balance: " + balance)
+				return
+			return
+		else:
+			bot.say("You do not have sufficient privileges.")
+			return
+	else:
+		bot.say("Please enter in format of .debit <user> <amount>")
+		return
+	con.close()
+
+@module.commands('newuser')
+def newuser (bot, trigger):
+	if trigger.group(3) and trigger.group(4) and trigger.group(5):
+		tnick, tamt = trigger.group(2).split(None, 2)
+		nick = re.sub("[^a-zA-Z0-9]","", tnick)
+		amt = re.sub("[^0-9]","", tamt)
+		id = re.sub("[^0-9]","", tid)
+	else:
+		bot.say("Please enter in format of .newuser <btn id> <user> <amount>")
+		return
+	try:
+		con = mdb.connect('localhost', 'casino', 'btncasino', 'casino');
+	except:
+		print "Error: Unable to connect to user settings DB."
+		bot.say("Error: Unable to connect to DB.")
+		return
+	
+	if nick and amt and id:
+		cur = con.cursor()
+		cur.execute("SELECT role FROM user WHERE nick = '" + trigger.nick + "';")
+		row = cur.fetchone()
+		if int(row[0]) >= staff:
+			cur.execute("INSERT INTO btn (btn_id, name) VALUES ('" + id + "', '" + nick + "');")
+			cur.execute("INSERT INTO user (btn_id, nick, role) VALUES ('" + id + "', '" + nick + "', '1');")
+			cur.execute("INSERT INTO wallet (id, balance) VALUES ('" + id + "', '" + amt + "');")
+			con.commit()
+			cur.execute("SELECT balance FROM wallet WHERE id = '" + id + "';")
+			row = cur.fetchone()
+			balance = str(row[0])
+			bot.say("User: " + nick + "(" + id + ") created with a balance of " + amt)
+			return
+		else:
+			bot.say("You do not have sufficient privileges.")
+			return
+	else:
+		bot.say("Please enter in format of .newuser <btn id> <user> <amount>")
+		return
+	con.close()
---
+++ willie/dice.py
@@ -0,0 +1,63 @@
+from willie import module
+from random import randint
+import MySQLdb as mdb
+import sys
+import re
+
+@module.commands('dice')
+def dice (bot, trigger):
+	try:
+		con = mdb.connect('localhost', 'casino', 'btncasino', 'casino');
+		
+	except:
+		print "Error: Unable to connect to user settings DB."
+		bot.say("Error: Unable to connect to DB.")
+		return
+
+	nick = trigger.nick
+	cur = con.cursor()
+	cur.execute("SELECT u.btn_id, w.balance FROM user u, wallet w WHERE u.nick = '" + nick + "' AND w.id = u.btn_id;")
+		
+	row = cur.fetchone()
+	if row is None:
+		bot.say(nick + " does not exist in the database.")
+		return
+	elif trigger.group(3) and trigger.group(4):
+		id = str(row[0])
+		balance = int(row[1])
+		amt2, bet = trigger.group(2).split(None, 1)
+		amt = int(amt2)
+		if amt > balance:
+			bot.say("You have not entered a valid amount, please try again.")
+			return
+		elif bet == 'high' or bet == 'low' :
+			d1 = randint(1,6)
+			d2 = randint(1,6)
+			d3 = randint(1,6)
+			sum = d1 + d2 + d3
+			if sum >= 10:
+				result = 'high'
+			else:
+				result = 'low'
+			t1 = d1, d2, d3
+			t2 = sum, result
+			s1 = str(t1)
+			s2 = str(t2)
+			s3 = re.sub("[^a-zA-Z1-9, ]","", s2)
+			if bet == result:
+				nb = balance + amt
+				bot.say(s1 + " is " + s3 + ". You win " + amt2 + ".")
+			else:
+				nb = balance - amt
+				bot.say(s1 + " is " + s3 + ". You lose " + amt2 + ".")
+			nb2 = str(nb)
+			cur.execute("UPDATE wallet SET balance = '" + nb2 + "' WHERE id = '" + id + "';")
+			con.commit()
+			return
+		else:
+			bot.say("Please enter your bet in form of .dice <amount> <high/low>")
+			return
+	else:
+		bot.say("Please enter your bet in form of .dice <amount> <high/low>")
+		return
+	con.close()
